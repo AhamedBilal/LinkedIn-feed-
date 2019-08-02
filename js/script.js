3ad9ffe8ba936cfd4b594698034002a994d8d7c6
Ahamed Bilal <bilalazmy0@gmail.com>
2019-08-02 20:13:17 +0530

    v1

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$('#post-start-btn').click(function () {
+    $("#my-module").css("display", "flex");
+});
+
+$('.mm-head span').click(function () {
+    $("#my-module").css("display", "none");
+});
+
+$('#add-img-btn').click(function () {
+    $("#img-chooser-real").click();
+});
+
+$("")
+
+
